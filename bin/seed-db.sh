4c228b122da25c97bc2864c65e0591dbfd4d351a
gmega <giuliano.mega@gmail.com>
2019-07-10 17:13:26 -0300

    proper volume management for call db

--- bin/seed-db.sh
+++ bin/seed-db.sh
@@ -2,6 +2,13 @@
 #
 # Seeds the database with the base data required for Callblocker to function.
 
+set -e
+
+if [[ -e /data/seed-mark ]]; then
+    echo "Database already seeded."
+    exit 2
+fi
+
 cd ${APP_FOLDER}
 
 echo "Applying migrations."
@@ -11,3 +18,5 @@ python ./manage.py migrate
 echo "Seeding database."
 
 python ./manage.py loaddata ./callblocker/blocker/fixtures/initial.yaml
+
+touch /data/seed-mark
